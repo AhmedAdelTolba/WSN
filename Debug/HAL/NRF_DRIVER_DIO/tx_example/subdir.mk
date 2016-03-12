09382a2c073717275b5c43cb077da9044f957405
m.mamdooh <m.mamdooh@mamdooh>
2016-03-12 18:13:42 +0200

    TX_DIO

---
+++ Debug/HAL/NRF_DRIVER_DIO/tx_example/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../HAL/NRF_DRIVER_DIO/tx_example/main.c 
+
+OBJS += \
+./HAL/NRF_DRIVER_DIO/tx_example/main.o 
+
+C_DEPS += \
+./HAL/NRF_DRIVER_DIO/tx_example/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+HAL/NRF_DRIVER_DIO/tx_example/%.o: ../HAL/NRF_DRIVER_DIO/tx_example/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega16 -DF_CPU=1000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
