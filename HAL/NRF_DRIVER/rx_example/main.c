2983b2bd0cdd09be87d8f89c32bb3859522d1556
m.mamdooh <m.mamdooh@mamdooh>
2016-03-13 14:00:10 +0200

    ubdated

--- HAL/NRF_DRIVER/rx_example/main.c
+++ HAL/NRF_DRIVER/rx_example/main.c
@@ -1,6 +1,6 @@
 /*
 * ----------------------------------------------------------------------------
-* “THE COFFEEWARE LICENSE” (Revision 1):
+* â€œTHE COFFEEWARE LICENSEâ€� (Revision 1):
 * <ihsan@kehribar.me> wrote this file. As long as you retain this notice you
 * can do whatever you want with this stuff. If we meet some day, and you think
 * this stuff is worth it, you can buy me a coffee in return.
@@ -26,39 +26,9 @@
 /* ------------------------------------------------------------------------- */
 /* Printing functions */
 /* ------------------------------------------------------------------------- */
-#include "./util/xprintf.h"
+
 /* ------------------------------------------------------------------------- */
-void uart_init()
-{
-    uart_pin_output();
-}
-/* ------------------------------------------------------------------------- */
-void uart_put_char(uint8_t tx)
-{
-    uint8_t i;
 
-    /* Start condition */
-    uart_clr_pin();
-    uart_bit_dly();
-
-    for(i=0;i<8;i++)
-    {
-        if(tx & (1<<i))
-        {
-            uart_set_pin();
-        }
-        else
-        {
-            uart_clr_pin();
-        }
-
-        uart_bit_dly();
-    }
-
-    /* Stop condition */
-    uart_set_pin();
-    uart_bit_dly();
-}
 /* ------------------------------------------------------------------------- */
 uint8_t temp;
 uint8_t q = 0;
@@ -68,14 +38,6 @@ uint8_t rx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
 /* ------------------------------------------------------------------------- */
 int main()
 {
-    /* init the software uart */
-    uart_init();
-
-    /* init the xprintf library */
-    xdev_out(uart_put_char);
-
-    /* simple greeting message */
-    xprintf("\r\n> RX device ready\r\n");
 
     /* init hardware pins */
     nrf24_init();
