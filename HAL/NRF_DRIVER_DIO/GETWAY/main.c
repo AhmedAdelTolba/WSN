8f21de5afd8681b30aaae513f7ec1053f23a322e
m.mamdooh <m.mamdooh@mamdooh>
2016-04-12 11:50:35 +0200

    ubdate 4

--- HAL/NRF_DRIVER_DIO/GETWAY/main.c
+++ HAL/NRF_DRIVER_DIO/GETWAY/main.c
@@ -20,7 +20,7 @@ uint8_t data_array[4];
 uint8_t akc_array[4]={255,255,255,255};
 uint8_t tx_addressNODE[5] = {0xcf,0xcf,0xcf,0xcf,0xcf}; // bet7ot el address beta3 el module elly bteb3atlo lw enta tx
 uint8_t tx_addressCOR[5] = {0xff,0xff,0xff,0xff,0xff};
-uint8_t rx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7}; // bt7ot el address bta3ak law enta rx
+uint8_t rx_address[5] = {0xAA,0xAA,0xAA,0xAA,0xAA}; // bt7ot el address bta3ak law enta rx
 uint8_t rx_arr[4]={0};
 uint32_t x ;
 uint8_t counter=0 ,flag=0 ;
@@ -52,7 +52,7 @@ int main()
 //
 //        /* Automatically goes to TX mode */
         nrf24_powerUpRx();
-        	_delay_ms(200);
+        	_delay_ms(2000);
   if(nrf24_dataReady())
   {
 	   nrf24_getData(rx_arr);
@@ -62,6 +62,7 @@ int main()
 		nrf24_send(rx_arr);
         while(nrf24_isSending());
         count=1 ;
+        DIO_u8WritePortVal(2,rx_arr[1]);
 	   }
 	   else if (count==1)
 	   {
