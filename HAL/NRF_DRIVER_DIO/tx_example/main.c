81bb7c52fa6644a1a17998d8ff358653bdab3bad
m.mamdooh <m.mamdooh@mamdooh>
2016-04-12 11:40:10 +0200

    commit 3

--- HAL/NRF_DRIVER_DIO/tx_example/main.c
+++ HAL/NRF_DRIVER_DIO/tx_example/main.c
@@ -18,7 +18,7 @@ uint8_t temp;
 uint8_t q = 1;
 uint8_t data_array[4];
 uint8_t akc_array[4]={255,255,255,255};
-uint8_t tx_address[5] = {0xAA,0xFF,0x12,0xCB,0x57}; // bet7ot el address beta3 el module elly bteb3atlo lw enta tx
+uint8_t tx_address[5] = {0xAA,0xAA,0xAA,0xAA,0xAA}; // bet7ot el address beta3 el module elly bteb3atlo lw enta tx
 uint8_t rx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7}; // bt7ot el address bta3ak law enta rx
 uint8_t rx_arr[4]={0};
 uint32_t x ;
@@ -76,7 +76,7 @@ void Auto_ACK()
 while(counter<mx_retransmetion)
 {
 	nrf24_powerUpRx();
-	_delay_ms(200);
+	_delay_ms(2000);
 
 		if(nrf24_dataReady())
 		{
