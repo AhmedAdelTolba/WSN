3179c2c4f3a59cf6691ef9660be20128d6907e32
m.mamdooh <m.mamdooh@mamdooh>
2016-04-13 13:29:18 +0200

    gg

--- HAL/NRF_DRIVER_DIO/GETWAY/main.c
+++ HAL/NRF_DRIVER_DIO/GETWAY/main.c
@@ -56,13 +56,14 @@ int main()
   if(nrf24_dataReady())
   {
 	   nrf24_getData(rx_arr);
-	   if (count==0)
+	   DIO_u8WritePortVal(2,rx_arr[1]);
+	   if (1)
 	   {
 		nrf24_tx_address(tx_addressNODE);
 		nrf24_send(rx_arr);
         while(nrf24_isSending());
         //count=1 ;
-      DIO_u8WritePortVal(2,0xFE);
+
 	   }
 	   else if (count==1)
 	   {
