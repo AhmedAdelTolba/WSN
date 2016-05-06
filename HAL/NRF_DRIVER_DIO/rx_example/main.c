cfc44bf6b35976d82e789663212d17516d3bc613
Ahmed <Ahmed@Ahmed-PC>
2016-05-06 13:54:30 +0200

    ay hatsh

--- HAL/NRF_DRIVER_DIO/rx_example/main.c
+++ HAL/NRF_DRIVER_DIO/rx_example/main.c
@@ -6,7 +6,7 @@
 
 int main()
     {
-//    uint8_t counter = 0;
+    uint8_t counter =2 ;
 
     uint16_t outer_counter = 0;
 
@@ -14,20 +14,19 @@ int main()
 
     uint8_t tx_address[5] =
 	{
-	0xD7, 0xD7, 0xD7, 0xD7, 0xD7
+//		0xee, 0xff, 0x12, 0xcb, 0x57
+		0xaa,0xff,0x12,0xcb,0x57
 	};
-//    uint8_t rx_address[5] =  //////////my address as a reciever
-//		{
-//		0xaa, 0xff, 0x12, 0xcb, 0x57
-//		};
     uint8_t rx_address[5] =  //////////my address as a reciever
-   		{
-   		0xcf, 0xcf, 0xcf, 0xcf, 0xcf
-   		};
+		{
+		0xaa, 0xee, 0x12, 0xcb, 0x57
+//			0xaa, 0xaa, 0xaa, 0xaa, 0xaa
+
+		};
 
     uint8_t sent_packet[4] =
 	{
-	1, 1, 1, 1
+		0,0,0,0//0x44, 0x44, 0x44, 0x44
 	};
     ///////////////////
 
@@ -45,46 +44,32 @@ int main()
     nrf24_rx_address(rx_address);
     //set_bit(DDRC, 0);
 
+	DIO_u8WritePortVal(DIO_u8PORT3, 0x33);
+
     while (1)
 	{
 	outer_counter++;
 
-//	if(counter==10)
-//	    { sent_packet[0]=0;
-//	    counter=0;}
-//	else{}
-
 	if (nrf24_dataReady())
 	    {
-//	    counter++;
-//	    if (counter == 1)
-//		{
-//		PORTD = outer_counter;
-//		_delay_ms(10000);
-//
-//		PORTD = (outer_counter >> 8);
-//		_delay_ms(10000);
-//
-//		PORTD = outer_counter;
-//		_delay_ms(10000);
-//
-//		}
 
 	    nrf24_getData(data_array);
 
 	    for (int i = 0; i < 4; i++)
 		{
+		sent_packet[i]=counter;
 		//PORTD = ~data_array[i];
 		DIO_u8WritePortVal(DIO_u8PORT3, ~data_array[i]);
 		}
-	    _delay_ms(100);
+	    _delay_ms(30);
 
 	    nrf24_powerUpTx();
-
 	    nrf24_send(sent_packet);
 	    while (nrf24_isSending())
 		;
 	    nrf24_powerUpRx();
+
+	    counter++;
 	    }
 
 	}
